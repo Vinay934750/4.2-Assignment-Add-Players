35cb23961d69ff2afb06b1148f70c1b19a930604
Vinay934750 <145250405+Vinay934750@users.noreply.github.com>
2023-10-01 17:04:18 +0300

    Create Add.js

---
+++ Add.js
@@ -0,0 +1,10 @@
+ng new player-management-app
+
+ng generate component add-player
+ng generate component update-player
+ng generate component delete-player
+ng generate component query-1
+ng generate component query-2
+ng generate component query-3
+ng generate component query-4
+ng generate component query-5
